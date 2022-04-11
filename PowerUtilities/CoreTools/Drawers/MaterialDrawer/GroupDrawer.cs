e62eee2971f47e1f02049cb7c49b9d1af11ab766
ken <redcool007@qq.com>
2022-04-11 13:41:57 +0800

    update GroupDrawer.cs

--- PowerUtilities/CoreTools/Drawers/MaterialDrawer/GroupDrawer.cs
+++ PowerUtilities/CoreTools/Drawers/MaterialDrawer/GroupDrawer.cs
@@ -5,6 +5,7 @@ namespace PowerUtilities
     using System.Collections.Generic;
     using UnityEditor;
     using UnityEngine;
+    using System.Linq;
 
     /// <summary>
     /// like this:
@@ -20,29 +21,38 @@ namespace PowerUtilities
     {
         static Dictionary<string, bool> groupDict = new Dictionary<string, bool>();
         string groupName;
+        string keyword;
         bool isGroupLabel;
 
         public static Dictionary<string, bool> GroupDict => groupDict;
-        public Group() : this("DefaultGroup", "")
-        {
-
-        }
-        public Group(string groupName) : this(groupName, "")
-        {
-        }
-        public Group(string groupName, string groupLabel)
+        public Group() : this("DefaultGroup", "",""){}
+        public Group(string groupName) : this(groupName, "",""){}
+        public Group(string groupName, string groupLabel) :this(groupName,groupLabel,""){ }
+        public Group(string groupName, string groupLabel,string keyword)
         {
             this.groupName = groupName;
+            this.keyword = keyword;
             this.isGroupLabel = !string.IsNullOrEmpty(groupLabel) && groupLabel == "true";
 
             if (isGroupLabel)
-                groupDict[groupName] = false;
+                groupDict[groupName] = EditorPrefs.GetBool(groupName, false);
+
         }
 
         public override float GetPropertyHeight(MaterialProperty prop, string label, MaterialEditor editor)
         {
-            if (GroupDict[groupName])
+            if (isGroupLabel)
                 return base.GetPropertyHeight(prop, label, editor);
+            if (GroupDict[groupName])
+            {
+                var baseHeight = base.GetPropertyHeight(prop, label, editor);
+                if(prop.type == MaterialProperty.PropType.Texture)
+                {
+                    baseHeight *= 4;
+                }
+                return baseHeight;
+            }
+
             return 0;
         }
 
@@ -50,11 +60,13 @@ namespace PowerUtilities
         {
             if (isGroupLabel)
             {
-                DrawLabel(position, label);
+                DrawLabel(position, label,prop);
             }
             else
             {
+                EditorGUI.indentLevel++;
                 DrawContent(position, prop, label, editor);
+                EditorGUI.indentLevel--;
             }
         }
 
@@ -68,11 +80,29 @@ namespace PowerUtilities
                 editor.DefaultShaderProperty(position, prop, label.text);
             }
         }
-
-        private void DrawLabel(Rect position, GUIContent label)
+         
+        private void DrawLabel(Rect position, GUIContent label, MaterialProperty prop)
         {
+            EditorGUI.BeginChangeCheck();
             groupDict[groupName] = EditorGUI.BeginFoldoutHeaderGroup(position, groupDict[groupName], label);
             EditorGUI.EndFoldoutHeaderGroup();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                // write to register
+                EditorPrefs.SetBool(groupName, groupDict[groupName]);
+
+                // keyword
+                var isKeywordOn = groupDict[groupName];
+                var mats = prop.targets.Select(t => (Material)t);
+                foreach (var mat in mats)
+                {
+                    if (isKeywordOn)
+                        mat.EnableKeyword(keyword);
+                    else
+                        mat.DisableKeyword(keyword);
+                }
+            }
         }
     }
 
