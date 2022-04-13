112e9c306746d34bd07878b67d635c6cabdf779a
ken <redcool007@qq.com>
2022-04-13 18:33:58 +0800

    add asmdef remove namespace

--- PowerUtilities/CoreTools/Drawers/PropertyDrawer/DisplayNamePropertyDrawer.cs
+++ PowerUtilities/CoreTools/Drawers/PropertyDrawer/DisplayNamePropertyDrawer.cs
@@ -5,46 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace PowerUtilities
-{
 #if UNITY_EDITOR
     using UnityEditor;
 
-    [CustomPropertyDrawer(typeof(DisplayName))]
-    public class DisplayNameDrawer : PropertyDrawer
+[CustomPropertyDrawer(typeof(DisplayName))]
+public class DisplayNameDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            //base.OnGUI(position, property, label);
-            var attr = attribute as DisplayName;
-            var ranges = fieldInfo.GetCustomAttributes(typeof(RangeAttribute), true);
+        //base.OnGUI(position, property, label);
+        var attr = attribute as DisplayName;
+        var ranges = fieldInfo.GetCustomAttributes(typeof(RangeAttribute), true);
 
-            label.text = attr.Value;
-            if (ranges.Length > 0)
-            {
-                var range = ranges[0] as RangeAttribute;
-                EditorGUI.Slider(position, property, range.min, range.max, label);
-            }
-            else
-            {
-                EditorGUI.PropertyField(position, property, label);
-            }
+        label.text = attr.Value;
+        if (ranges.Length > 0)
+        {
+            var range = ranges[0] as RangeAttribute;
+            EditorGUI.Slider(position, property, range.min, range.max, label);
+        }
+        else
+        {
+            EditorGUI.PropertyField(position, property, label);
         }
     }
+}
 #endif
-    public class DisplayName : PropertyAttribute
+
+
+public class DisplayName : PropertyAttribute
+{
+    public string Value;
+
+    public DisplayName(string value)
     {
-        public string Value;
-
-        public DisplayName(string value)
-        {
-            this.Value = value;
-        }
-
-        public override string ToString()
-        {
-            return Value;
-        }
+        this.Value = value;
     }
 
+    public override string ToString()
+    {
+        return Value;
+    }
 }
