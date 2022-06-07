5efa4f17bbdd6507af22b27bdbcc5a1335fc8254
ken <redcool007>
2022-06-07 18:04:56 +0800

    add GameUtilsFramework

---
+++ PowerUtilities/GameUtilsFramework/AnimatorStates/SetStateVariables.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace BoxSoul
+{
+    [Serializable]
+    public class StateKeyValue
+    {
+        public enum When
+        {
+            Enter,Update,Exit
+        }
+        public string stateName;
+        public bool value;
+        public bool isTrigger;
+
+        [Header("When")]
+        public When when;
+
+    }
+    public class SetStateVariables : StateMachineBehaviour
+    {
+
+        public enum SendMessageDir
+        {
+            Self,Upward, Broadcast
+        }
+
+        [Header("State Parameters")]
+        public StateKeyValue[] keyValues;
+
+        [Header("Functions")]
+        public SendMessageDir messageDir = SendMessageDir.Self;
+        public string[] enterFunctionNames;
+        public string[] updateFunctionNames;
+        public string[] exitFunctionNames;
+
+        void UpdateStates(Animator animator,StateKeyValue.When when)
+        {
+            for (int i = 0; i < keyValues.Length; i++)
+            {
+                var kv = keyValues[i];
+                if (kv.when != when)
+                    continue;
+
+                if (kv.isTrigger)
+                    if (kv.value)
+                        animator.SetTrigger(kv.stateName);
+                    else
+                        animator.ResetTrigger(kv.stateName);
+                else
+                    animator.SetBool(kv.stateName, kv.value);
+            }
+        }
+
+        void InvokeFunctions(Animator anim, string[] functions)
+        {
+            for (int i = 0; i < functions.Length; i++)
+            {
+                var func = functions[i];
+
+                if (messageDir == SendMessageDir.Self)
+                    anim.SendMessage(func, SendMessageOptions.DontRequireReceiver);
+                else if (messageDir == SendMessageDir.Upward)
+                    anim.SendMessageUpwards(func, SendMessageOptions.DontRequireReceiver);
+                else
+                    anim.BroadcastMessage(func, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            UpdateStates(animator, StateKeyValue.When.Enter);
+            InvokeFunctions(animator,enterFunctionNames);
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            UpdateStates(animator, StateKeyValue.When.Update);
+            InvokeFunctions(animator, updateFunctionNames);
+        }
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            UpdateStates(animator, StateKeyValue.When.Exit);
+            InvokeFunctions(animator, exitFunctionNames);
+        }
+
+        // OnStateMove is called right after Animator.OnAnimatorMove()
+        //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that processes and affects root motion
+        //}
+
+        // OnStateIK is called right after Animator.OnAnimatorIK()
+        //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that sets up animation IK (inverse kinematics)
+        //}
+    }
+}
---
+++ PowerUtilities/GameUtilsFramework/AnimatorTools.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameUtilsFramework
+{
+    public static class AnimatorTools
+    {
+        public static float QuantifyInputValue(float v)
+        {
+            if (v > 0.02f && v < 0.55f)
+                return 0.5f;
+            if (v > 0.55f)
+                return 1;
+            if (v < -0.02f && v > -0.55f)
+                return -0.5f;
+            if (v < -0.55f)
+                return -1;
+            return v;
+        }
+    }
+}
