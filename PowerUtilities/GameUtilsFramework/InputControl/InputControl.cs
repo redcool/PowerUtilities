aeebceaf9a1835ad182db3e41e6402a31f2a4859
ken <redcool007>
2022-06-07 18:18:57 +0800

    update GameUtilsFramework

--- PowerUtilities/GameUtilsFramework/InputControl/InputControl.cs
+++ PowerUtilities/GameUtilsFramework/InputControl/InputControl.cs
@@ -3,34 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputControl : MonoBehaviour
+namespace GameUtilsFramework
 {
-    public Vector2 movement;
-    public Vector2 look;
-    public bool isSprint,
-        LB, LT, RB, RT
-        ;
 
-    public float movementLength;
-    public void OnMovement(InputValue v)
+    public class InputControl : MonoBehaviour
     {
-        movement = v.Get<Vector2>();
-        movementLength = movement.magnitude;
-    }
+        public Vector2 movement;
+        public Vector2 look;
+        public bool isSprint,
+            LB, LT, RB, RT
+            ;
 
-    public void OnLook(InputValue v)
-    {
-        look = v.Get<Vector2>();
-    }
+        public float movementLength;
+        public void OnMovement(InputValue v)
+        {
+            movement = v.Get<Vector2>();
+            movementLength = movement.magnitude;
+        }
 
-    public void OnSprint(InputValue v)
-    {
-        isSprint = v.isPressed;
-    }
+        public void OnLook(InputValue v)
+        {
+            look = v.Get<Vector2>();
+        }
 
-    public void OnLB(InputValue v) => LB = v.isPressed;
-    public void OnLT(InputValue v) => LT = v.isPressed;
-    public void OnRB(InputValue v) => RB = v.isPressed;
-    public void OnRT(InputValue v) => RT = v.isPressed;
+        public void OnSprint(InputValue v)
+        {
+            isSprint = v.isPressed;
+        }
+
+        public void OnLB(InputValue v) => LB = v.isPressed;
+        public void OnLT(InputValue v) => LT = v.isPressed;
+        public void OnRB(InputValue v) => RB = v.isPressed;
+        public void OnRT(InputValue v) => RT = v.isPressed;
+
+    }
 
 }
