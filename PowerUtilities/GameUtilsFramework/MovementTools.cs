c60ddd20db5c52149ea5ad14386e8f3aa7205edc
ken <redcool007>
2022-06-09 15:53:41 +0800

    update GameUtilsFramework

--- PowerUtilities/GameUtilsFramework/MovementTools.cs
+++ PowerUtilities/GameUtilsFramework/MovementTools.cs
@@ -10,9 +10,9 @@ namespace GameUtilsFramework
     public static class MovementTools
     {
 
-        public static bool AdjustVelocityToSlope(Transform playerTr,ref Vector3 velocity,LayerMask layer)
+        public static bool AdjustVelocityToSlope(Transform playerTr,ref Vector3 velocity,float distance,LayerMask layer)
         {
-            if(Physics.Raycast(playerTr.position+Vector3.up,Vector3.down,out var hit,float.MaxValue,layer))
+            if(Physics.Raycast(playerTr.position+Vector3.up*0.1f,Vector3.down,out var hit,distance,layer))
             {
                 if (hit.normal != Vector3.up)
                 {
