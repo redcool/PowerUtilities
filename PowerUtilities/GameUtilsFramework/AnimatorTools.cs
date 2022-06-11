515fad2b91b409adfa638700ca140d47772e43ee
ken <redcool007>
2022-06-12 00:26:30 +0800

    update animatorTools

--- PowerUtilities/GameUtilsFramework/AnimatorTools.cs
+++ PowerUtilities/GameUtilsFramework/AnimatorTools.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace GameUtilsFramework
 {
     public static class AnimatorTools
     {
+        static Dictionary<string, int> animatorLayerIdDict = new Dictionary<string, int>();
+
+        public static int GetLayerIndexFromCache(this Animator anim, string layerName)
+        {
+           if(!animatorLayerIdDict.TryGetValue(layerName,out var id))
+            {
+                id = animatorLayerIdDict[layerName] = anim.GetLayerIndex(layerName);
+            }
+            return id;
+        }
+
         public static float QuantifyInputValue(float v)
         {
             if (v > 0.02f && v < 0.55f)
@@ -21,5 +33,20 @@ namespace GameUtilsFramework
             return v;
         }
 
+        public static AnimatorStateInfo GetCurrentStateInfo(this Animator anim,string layerName)
+        {
+            return anim.GetCurrentAnimatorStateInfo(anim.GetLayerIndexFromCache(layerName));
+        }
+
+        public static bool IsInState(this Animator anim,string layerName,string stateName)
+        {
+            return GetCurrentStateInfo(anim,layerName).IsName(stateName);
+        }
+
+        public static void PlayAnimSetBool(this Animator anim,string stateName, string varName, bool varValue)
+        {
+            anim.SetBool(varName, varValue);
+            anim.CrossFade(stateName, 0.2f);
+        }
     }
 }
