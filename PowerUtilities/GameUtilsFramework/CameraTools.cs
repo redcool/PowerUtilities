ea58f2322e725b7be7d34b14c42c5fcf9c0269f2
ken <redcool007@qq.com>
2022-06-22 09:04:49 +0800

    update cameraTools

--- PowerUtilities/GameUtilsFramework/CameraTools.cs
+++ PowerUtilities/GameUtilsFramework/CameraTools.cs
@@ -16,5 +16,23 @@ namespace GameUtilsFramework
             dir.Normalize();
             return dir;
         }
+
+        public static Transform RaycastTarget(Transform camTr,float maxDistance,LayerMask layer,Predicate<Collider> condition)
+        {
+            var ray = new Ray(camTr.position, camTr.forward);
+            //var isHitted = Physics.Raycast(ray, out var hit, maxDistance, layer);
+            var hits = Physics.RaycastAll(ray, maxDistance, layer);
+            if (hits.Length == 0)
+                return null;
+
+            for (int i = 0; i < hits.Length; i++)
+            {
+                var hit = hits[i];
+                if (condition(hit.collider))
+                    return hit.collider.transform;
+            }
+
+            return null;
+        }
     }
 }
