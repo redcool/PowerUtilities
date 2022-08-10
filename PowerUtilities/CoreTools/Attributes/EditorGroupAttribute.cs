57336b104ca21ac9aa710bfc63530d47f8e2dc67
ken <redcool007@qq.com>
2022-08-10 18:35:44 +0800

    refactor Attributes

---
+++ PowerUtilities/CoreTools/Attributes/EditorGroupAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace PowerUtilities.Drawers
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Show toogle group in inspector
+    /// 
+    /// [EditorGroup("Fog",true)] public bool _IsGlobalFogOn;
+    /// [EditorGroup("Fog")][Range(0, 1)] public float _GlobalFogIntensity = 1;
+    /// </summary>
+    public class EditorGroupAttribute : PropertyAttribute
+    {
+        public string groupName;
+        public bool isHeader;
+
+        public EditorGroupAttribute(string groupName,bool isHeader=false)
+        {
+            this.isHeader = isHeader;
+            this.groupName=groupName;
+        }
+    }
+
+}
--- PowerUtilities/EditorCoreTools/Drawers/AttributeDrawers/EditorGroupAttributeDrawer.cs
+++ PowerUtilities/EditorCoreTools/Drawers/AttributeDrawers/EditorGroupAttributeDrawer.cs
@@ -58,22 +58,6 @@ using UnityEditor;
         }
     }
 #endif
-    /// <summary>
-    /// Show toogle group in inspector
-    /// 
-    /// [EditorGroup("Fog",true)] public bool _IsGlobalFogOn;
-    /// [EditorGroup("Fog")][Range(0, 1)] public float _GlobalFogIntensity = 1;
-    /// </summary>
-    public class EditorGroupAttribute : PropertyAttribute
-    {
-        public string groupName;
-        public bool isHeader;
 
-        public EditorGroupAttribute(string groupName,bool isHeader=false)
-        {
-            this.isHeader = isHeader;
-            this.groupName=groupName;
-        }
-    }
 
 }
