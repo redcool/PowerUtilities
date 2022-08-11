a7e30f13d612af99c2fcfcc1b352c4c3b639148f
ken <redcool007@qq.com>
2022-08-11 16:29:43 +0800

    update LinqEx rename UpdateMaterialKeywordsWindow.cs

--- PowerUtilities/CoreTools/PathTools.cs
+++ PowerUtilities/CoreTools/PathTools.cs
@@ -9,7 +9,7 @@
     public static class PathTools
     {
 
-        public static string GetAssetDir(string assetPath,params object[] combineStrings)
+        public static string GetAssetDir(string assetPath,params string[] combineStrings)
         {
             if (string.IsNullOrEmpty(assetPath))
                 return "";
