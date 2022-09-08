dca47b2fcd67465199346eb7a68e1ae1c803b46e
ken <redcool007@qq.com>
2022-09-08 13:06:00 +0800

    fixed EditorCode   GroupDecorator/GroupSpaceDecorator.cs

--- PowerUtilities/EditorCoreTools/MaterialDrawers/GroupAPI/GroupDecorator/GroupSpaceDecorator.cs
+++ PowerUtilities/EditorCoreTools/MaterialDrawers/GroupAPI/GroupDecorator/GroupSpaceDecorator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,3 +37,4 @@ namespace PowerUtilities
         }
     }
 }
+#endif
