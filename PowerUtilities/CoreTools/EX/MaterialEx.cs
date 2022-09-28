0f76d7d50621cb8a1c3b290e1185f16ae3e4b308
ken <redcool007@qq.com>
2022-09-28 19:23:21 +0800

    add MaterialEx.cs

---
+++ PowerUtilities/CoreTools/EX/MaterialEx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PowerUtilities
+{
+    public static class MaterialEx
+    {
+        public static void SetKeyword(this Material mat, string keyword, bool isOn)
+        {
+            if (string.IsNullOrEmpty(keyword))
+                return;
+
+            var isOpened = mat.IsKeywordEnabled(keyword);
+            if (isOn == isOpened)
+                return;
+
+            if (isOn)
+                mat.EnableKeyword(keyword);
+            else
+                mat.DisableKeyword(keyword);
+        }
+    }
+}
