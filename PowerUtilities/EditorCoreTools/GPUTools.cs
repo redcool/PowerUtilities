4e3c4fa006272e0ec1f36f815ac88263e5f1dcab
ken <redcool007@qq.com>
2023-01-06 21:17:39 +0800

    v(2.0.2.7.4) update TextureTools

--- PowerUtilities/EditorCoreTools/GPUTools.cs
+++ PowerUtilities/EditorCoreTools/GPUTools.cs
@@ -1,9 +1,12 @@
 ﻿#if UNITY_EDITOR
 namespace PowerUtilities
 {
+    using System;
     using System.Collections;
     using System.Collections.Generic;
+    using Unity.Collections;
     using UnityEngine;
+    using UnityEngine.Rendering;
 
     public static class GPUTools
     {
@@ -30,6 +33,39 @@ namespace PowerUtilities
             RenderTexture.active = lastRT;
             RenderTexture.ReleaseTemporary(rt);
             return tex;
+        }
+
+        public static void AsyncGPUReadRenderTexture(RenderTexture rt, int pixelByteCount = 4, Action<byte[]> onDone = null)
+        {
+            if (onDone == null || !rt)
+                return;
+
+            var buffer = new NativeArray<byte>(rt.width * rt.height * pixelByteCount, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+
+            AsyncGPUReadback.RequestIntoNativeArray(ref buffer, rt, 0, request =>
+            {
+                if (request.hasError)
+                {
+                    buffer.Dispose();
+                    return;
+                }
+                using var enc = ImageConversion.EncodeNativeArrayToPNG(buffer, rt.graphicsFormat, (uint)rt.width, (uint)rt.height);
+                onDone(enc.ToArray());
+                buffer.Dispose();
+            });
+        }
+
+        public static void ReadRenderTexture(RenderTexture sourceTex,ref Texture2D targetTex)
+        {
+            if (!sourceTex)
+                return;
+            
+            if(!targetTex)
+                targetTex = new Texture2D(sourceTex.width, sourceTex.height, TextureFormat.ARGB32, false, true);
+
+            RenderTexture.active = sourceTex;
+            targetTex.ReadPixels(new Rect(0, 0, sourceTex.width, sourceTex.height), 0, 0, false);
+            RenderTexture.active = null;
         }
     }
 }
--- PowerUtilities/EditorCoreTools/TextureTools.cs
+++ PowerUtilities/EditorCoreTools/TextureTools.cs
@@ -84,6 +84,8 @@ namespace PowerUtilities
             return textureList;
         }
 
+        public static bool IsNeedCreateTexture(Texture t, int targetWidth, int targetHeight) => !(t && t.width == targetWidth && t.height == targetHeight);
+
     }
 }
 #endif
