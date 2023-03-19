bd7e2c230da41dc9803f142911e81bceb1636962
ken <redcool007@qq.com>
2023-03-19 17:27:49 +0800

    v(2.0.6.9) add RenderTargetIdentifierEx.cs

---
+++ PowerUtilities/CoreTools/EX/RenderTargetIdentifierEx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Rendering;
+
+namespace PowerUtilities
+{
+    public static class RenderTargetIdentifierEx
+    {
+        public static bool IsTargetIdEquals(this RenderTargetIdentifier a, RenderTargetIdentifier b)
+        {
+            var t = typeof(RenderTargetIdentifier);
+
+            var f = t.GetField("m_NameID", BindingFlags.Instance | BindingFlags.NonPublic);
+            var aId = (int)f.GetValue(a);
+            var bId = (int)f.GetValue(b);
+            return aId == bId;
+        }
+    }
+}
