2bc055713148a872d4ddb87d13bff7cf1c07aaf0
ken <redcool007@qq.com>
2023-04-18 00:02:16 +0800

    * v(2.0.6.23) rename shaderFeatures -> RenderFeatures add control

---
+++ PowerUtilities/RenderFeatures/Control/SRPRenderFeatureSO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace PowerUtilities.RenderFeatures
+{
+    [CreateAssetMenu(menuName = "SrpRenderFeatures/SRPRenderFeatureSO")]
+    public class SRPRenderFeatureSO : ScriptableObject
+    {
+
+    }
+}
