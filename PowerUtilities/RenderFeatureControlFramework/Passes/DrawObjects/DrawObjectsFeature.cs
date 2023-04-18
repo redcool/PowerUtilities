3f9bbbfd8a21810ba85847c6cdd51b783aee2d72
ken <redcool007@qq.com>
2023-04-18 23:39:15 +0800

    * v(2.0.7.4) add DrawObjects

---
+++ PowerUtilities/RenderFeatureControlFramework/Passes/DrawObjects/DrawObjectsFeature.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering.Universal.Internal;
+
+namespace PowerUtilities.RenderFeatures
+{
+    [CreateAssetMenu(menuName =SRP_FEATURE_MENU+ "/DrawObjectsFeature")]
+    public class DrawObjectsFeature : SRPFeature
+    {
+        public string profilerTag = "DrawObjectsFeature";
+        public string[] shaderTags = new[] {
+            "UniversalForwardOnly",
+            "UniversalForward",
+            "UniversalUnlit"
+        }; 
+        
+        public bool isOpaque;
+        public RenderQueueRange renderQueueRange = RenderQueueRange.opaque;
+        public LayerMask layers = -1;
+
+        public StencilStateData stencilData;
+
+        public override ScriptableRenderPass GetPass()
+        {
+            UniversalRenderPipelineAsset asset = UniversalRenderPipeline.asset;
+
+            var stencilState = StencilState.defaultValue;
+            stencilState.enabled = stencilData.overrideStencilState;
+            stencilState.SetCompareFunction(stencilData.stencilCompareFunction);
+            stencilState.SetFailOperation(stencilData.failOperation);
+            stencilState.SetPassOperation(stencilData.passOperation);
+            stencilState.SetZFailOperation(stencilData.zFailOperation);
+
+            var shaderTagIds = shaderTags
+                .Select(name => new ShaderTagId(name))
+                .ToArray();
+
+            return new DrawObjectsPass(profilerTag,shaderTagIds,isOpaque,renderPassEvent,renderQueueRange,layers,stencilState,stencilData.stencilReference);
+        }
+    }
+}
--- PowerUtilities/RenderFeatureControlFramework/SRPFeatureList.cs
+++ PowerUtilities/RenderFeatureControlFramework/SRPFeatureList.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿namespace PowerUtilities.RenderFeatures
+{
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace PowerUtilities.RenderFeatures
-{
+#if UNITY_EDITOR
+    using UnityEditor;
+    [CustomEditor(typeof(SRPFeatureList))]
+    public class SRPFeatureListEditor : PowerEditor<SRPFeatureList>
+    {
+        bool isFold;
+
+        public override void DrawInspectorUI(SRPFeatureList inst)
+        {
+            DrawDefaultGUI();
+
+            isFold = EditorGUILayout.Foldout(isFold, "Passes",true);
+            if (isFold)
+            {
+                for (int i = 0; i < inst.settingList.Count; i++)
+                {
+                    var feature = inst.settingList[i];
+                    
+                    EditorGUILayout.BeginHorizontal("Box");
+                    {
+                        var text = $"{i},{feature.name}";
+                        EditorGUILayout.LabelField(text);
+                        if (!feature.enabled)
+                        {
+                            EditorGUITools.DrawColorUI(() =>
+                            {
+                                EditorGUILayout.ObjectField(feature, feature.GetType(), allowSceneObjects: false, GUILayout.Height(18));
+
+                            },Color.gray, GUI.color);
+                        }
+                        else
+                        {
+                            EditorGUILayout.ObjectField(feature, feature.GetType(), allowSceneObjects: false,GUILayout.Height(18));
+                        }
+
+                    }
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+        }
+    }
+#endif
+
     [CreateAssetMenu(menuName = "SrpRenderFeatures/SRPSettingList")]
     public class SRPFeatureList : ScriptableObject
     {
