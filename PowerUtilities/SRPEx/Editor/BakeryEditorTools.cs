b28f2a190a0c583a1d4d1da29688edc2483e55d5
ken <redcool007@qq.com>
2023-05-19 16:36:19 +0800

    v(2.0.9.23) update BakeryEditorTools, use serializedObject

--- PowerUtilities/SRPEx/Editor/BakeryEditorTools.cs
+++ PowerUtilities/SRPEx/Editor/BakeryEditorTools.cs
@@ -1,6 +1,7 @@
 #if UNITY_EDITOR && BAKERY_INCLUDED
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,58 +9,77 @@ namespace PowerUtilities
 {
     public static class BakeryEditorTools
     {
+        static Dictionary<Light, SerializedObject> lightDict = new Dictionary<Light, SerializedObject>();
 
-        public static Texture2D BakeryDefaultSpotCookieTexture => AssetDatabase.LoadAssetAtPath<Texture2D>(ftLightmaps.GetRuntimePath());
+        public static Texture2D BakeryDefaultSpotCookieTexture => AssetDatabaseTools.FindAssetsInProject<Texture2D>("ftUnitySpotTexture").FirstOrDefault();
+
+        public static SerializedObject GetBakeryLightSO(Light light,bool dontUseCache=false)
+        {
+            var isCached = lightDict.TryGetValue(light, out SerializedObject bakeryLightSo);
+
+            if(!isCached || dontUseCache)
+            {
+                var bakeryLight = light.GetComponents(typeof(MonoBehaviour))
+                .Where(mono => mono.GetType().Name.StartsWith("Bakery"))
+                .FirstOrDefault();
+
+                if(bakeryLight != null)
+                    bakeryLightSo = lightDict[light] = new SerializedObject(bakeryLight);
+            }
+            
+            return bakeryLightSo;
+        }
 
         public static bool SyncBakeryLight(Light light)
         {
-            var bLight = light.GetComponent<BakeryDirectLight>();
-            if (bLight)
-            {
-                bLight.color = light.color;
-                bLight.intensity = light.intensity;
-                bLight.indirectIntensity = light.bounceIntensity;
-                if (RenderPipelineTools.IsHDRenderPipeline())
-                    bLight.intensity /= Mathf.PI;
-            }
+            //var bLightSo = GetBakeryLightSO(light);
+            var bLightSo = light.GetComponents(typeof(MonoBehaviour))
+                .Where(comp => comp.GetType().Name.StartsWith("Bakery"))
+                .Select(comp => new SerializedObject(comp))
+                .FirstOrDefault()
+                ;
 
-            var pLight = light.GetComponent<BakeryPointLight>();
-            if (pLight)
-            {
-                pLight.color = light.color;
-                pLight.intensity = light.intensity;
-                pLight.indirectIntensity = light.bounceIntensity;
+            if (bLightSo == null)
+                return false;
 
-                pLight.cutoff = light.range;
-                pLight.angle = light.spotAngle;
+            bLightSo.UpdateIfRequiredOrScript();
+            // update BakeryDirectLight
+            bLightSo.FindProperty("color").colorValue = light.color;
+            bLightSo.FindProperty("intensity").floatValue = light.intensity;
+            bLightSo.FindProperty("indirectIntensity").floatValue = light.bounceIntensity;
+            if (RenderPipelineTools.IsHDRenderPipeline())
+                bLightSo.FindProperty("intensity").floatValue = light.intensity/Mathf.PI;
+
+            //update BakeryPointLight
+            if (light.type == LightType.Point || light.type == LightType.Spot)
+            {
+                bLightSo.FindProperty("cutoff").floatValue = light.range;
+                bLightSo.FindProperty("angle").floatValue = light.spotAngle;
 
                 if (light.type == LightType.Point)
                 {
-                    pLight.projMode = light.cookie == null ? BakeryPointLight.ftLightProjectionMode.Omni : BakeryPointLight.ftLightProjectionMode.Cubemap;
-                    pLight.cubemap = light.cookie as Cubemap;
-
+                    //BakeryPointLight.ftLightProjectionMode
+                    bLightSo.FindProperty("projMode").enumValueIndex = light.cookie == null ? 0 : 2;//BakeryPointLight.ftLightProjectionMode.Omni : BakeryPointLight.ftLightProjectionMode.Cubemap;
+                    bLightSo.FindProperty("cubemap").objectReferenceValue =  light.cookie as Cubemap;
                 }
                 else if (light.type == LightType.Spot)
                 {
-                    pLight.projMode = BakeryPointLight.ftLightProjectionMode.Cookie;
-                    pLight.cookie = light.cookie == null ? BakeryDefaultSpotCookieTexture : (Texture2D)light.cookie;
+                    bLightSo.FindProperty("projMode").enumValueIndex = 1;//BakeryPointLight.ftLightProjectionMode.Cookie
+                    bLightSo.FindProperty("cookie").objectReferenceValue = light.cookie == null ? BakeryDefaultSpotCookieTexture : (Texture2D)light.cookie;
                 }
 
                 if (RenderPipelineTools.IsHDRenderPipeline() || RenderPipelineTools.IsUniversalPipeline())
                 {
-                    pLight.realisticFalloff = true;
-                    pLight.falloffMinRadius = 0.01f;
-                    pLight.projMode = BakeryPointLight.ftLightProjectionMode.Cone;
-                    pLight.innerAngle = light.innerSpotAngle;
-                }
-
-                if (RenderPipelineTools.IsHDRenderPipeline())
-                {
-                    pLight.intensity /= Mathf.PI;
+                    bLightSo.FindProperty("realisticFalloff").boolValue = true;
+                    bLightSo.FindProperty("falloffMinRadius").floatValue = 0.01f;
+                    bLightSo.FindProperty("projMode").enumValueIndex = 4;//BakeryPointLight.ftLightProjectionMode.Cone;
+                    bLightSo.FindProperty("innerAngle").floatValue = light.innerSpotAngle;
                 }
             }
 
-            return bLight || pLight;
+            bLightSo.ApplyModifiedProperties();
+
+            return true;
         }
     }
 }
