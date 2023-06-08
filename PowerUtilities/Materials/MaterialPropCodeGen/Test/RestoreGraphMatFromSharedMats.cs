991518c7b2a8f9a2c30d166cb6e921d9737110d8
ken <redcool007@qq.com>
2023-06-08 16:49:42 +0800

    v(2.0.9.66) update RestoreGraphMatFromShared, only open close prefab

--- PowerUtilities/Materials/MaterialPropCodeGen/Test/RestoreGraphMatFromSharedMats.cs
+++ PowerUtilities/Materials/MaterialPropCodeGen/Test/RestoreGraphMatFromSharedMats.cs
@@ -10,27 +10,37 @@ namespace PowerUtilities
 {
     public class RestoreGraphMatFromSharedMats
     {
+
         [MenuItem(UIMaterialPropCodeGen.MENU_ROOT+"/Tools/"+nameof(RestoreGraphMatFromSharedMats))]
         static void Restore()
         {
             var folders = SelectionTools.GetSelectedFolders();
             var updaters = AssetDatabaseTools.FindComponentsInProject<BaseUIMaterialPropUpdater>("", folders);
-            var count = 0;
+            var q = AssetDatabaseTools.FindAssetsInProject<GameObject>("", folders)
+                .Where(go => go.GetComponentInChildren<BaseUIMaterialPropUpdater>());
 
-            updaters.ForEach(updater => {
-                var prefab = PrefabUtility.GetCorrespondingObjectFromOriginalSource(updater.gameObject);
-                
+            q.ForEach(go =>
+            {
+                var prefab = PrefabUtility.GetCorrespondingObjectFromOriginalSource(go);
+
                 PrefabTools.ModifyPrefab(prefab, (go) =>
                 {
+                    /*
                     var updaters = go.GetComponentsInChildren<BaseUIMaterialPropUpdater>();
+                    var outerPrefab = PrefabUtility.GetCorrespondingObjectFromOriginalSource(go);
                     updaters.ForEach(updater =>
                     {
-                        if (updater && updater.RestoreGraphMatFromShared())
-                            count++;
+                        var innerPrefab = PrefabUtility.GetCorrespondingObjectFromOriginalSource(updater.gameObject);
+                        if (innerPrefab == outerPrefab)
+                        {
+                            if (updater.RestoreGraphMatFromShared())
+                                count++;
+                        }
                     });
+                    */
                 });
             });
-            Debug.Log($"RestoreGraphMatFromSharedMats,prefabs{updaters.Length}, modified : {count}");
+            Debug.Log($"RestoreGraphMatFromSharedMats,prefabs{q.Count()}");
         }
     }
 }
