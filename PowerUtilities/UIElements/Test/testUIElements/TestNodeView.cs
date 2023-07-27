44afeab277160eab9bbdfd4d6fa879448cfe8f0b
ken <redcool007@qq.com>
2023-07-27 20:31:13 +0800

    v(2.0.11.13) update TestNodeView

--- PowerUtilities/UIElements/Test/testUIElements/TestNodeView.cs
+++ PowerUtilities/UIElements/Test/testUIElements/TestNodeView.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEditor;
@@ -19,15 +20,21 @@ namespace PowerUtilities.UIElements
         {
             public Orientation orientation = Orientation.Vertical;
             public Port.Capacity capacity = Port.Capacity.Single;
+            public Direction direction = Direction.Input;
             public Type portType = typeof(bool);
             public string portName;
+
+            /// <summary>
+            /// Style Sheet
+            /// </summary>
+            public FlexDirection flexDirection = FlexDirection.Column;
         }
 
-        public static Lazy<string> lazyNodeViewUssPath = new Lazy<string>(()=> AssetDatabaseTools.FindAssetPath("TestNodeView","uxml"));
+        public static Lazy<string> lazyNodeViewUssPath = new Lazy<string>(() => AssetDatabaseTools.FindAssetPath("TestNodeView", "uxml"));
 
         public PortInfo
             inputPortInfo = new PortInfo(),
-            outputPortInfo = new PortInfo()
+            outputPortInfo = new PortInfo { direction = Direction.Output,flexDirection = FlexDirection.ColumnReverse}
         ;
 
         public Port input, output;
@@ -37,7 +44,7 @@ namespace PowerUtilities.UIElements
             this.title = "test node";
             this.viewDataKey = GUID.Generate().ToString();
 
-            CreatePorts();
+            CreatePortsAndAdd();
 
             SetupClasses();
         }
@@ -47,23 +54,33 @@ namespace PowerUtilities.UIElements
             AddToClassList("action");
         }
 
+
+
         /// <summary>
         /// create input and output port
         /// </summary>
-        public void CreatePorts()
-        {
-            input = InstantiatePort(inputPortInfo.orientation, Direction.Input, inputPortInfo.capacity, inputPortInfo.portType);
-            inputContainer.Clear();
-            inputContainer.Add(input);
-            input.portName = inputPortInfo.portName;
-            input.style.flexDirection = FlexDirection.Column;
+        public void CreatePortsAndAdd()
+        {   var input = CreatePort(inputPortInfo);
+            inputContainer.Add(true, input);
+         
 
-            output = InstantiatePort(outputPortInfo.orientation, Direction.Output, outputPortInfo.capacity, outputPortInfo.portType);
-            outputContainer.Clear();
-            outputContainer.Add(output);
-            output.portName = outputPortInfo.portName;
-            output.style.flexDirection = FlexDirection.ColumnReverse;
+            var output = CreatePort(outputPortInfo);
+            outputContainer.Add(true, output);
         }
+
+        public static Port CreatePort(string portName, Orientation orientation, Direction direction, Port.Capacity capacity, Type portType, FlexDirection flexDirection)
+        {
+            var port = Port.Create<Edge>(orientation, direction, capacity, portType);
+            port.portName = portName;
+            port.style.flexDirection = flexDirection;
+            return port;
+        }
+
+        public static Port CreatePort(PortInfo info)
+        {
+            return CreatePort(info.portName, info.orientation, info.direction, info.capacity, info.portType, info.flexDirection);
+        }
+
     }
 }
 #endif
