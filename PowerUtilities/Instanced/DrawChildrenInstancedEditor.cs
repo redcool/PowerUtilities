67a9335accdb44d6a2130c33fe399b988680c924
ken <redcool007@qq.com>
2023-08-17 18:57:44 +0800

    v(2.0.13.32) update editors

--- PowerUtilities/Instanced/DrawChildrenInstancedEditor.cs
+++ PowerUtilities/Instanced/DrawChildrenInstancedEditor.cs
@@ -54,7 +54,7 @@ namespace PowerUtilities
             {
                 isActive = !isActive;
                 //inst.gameObject.SetChildrenActive(isChildrenActive);
-                inst.drawInfoSO.SetupRenderers(inst.gameObject, true);
+                inst.drawInfoSO.SetupRenderers(inst.gameObject);
                 inst.drawInfoSO.SetRendersActive(isActive);
             }
             if (GUILayout.Button("Delete Children"))
@@ -64,7 +64,7 @@ namespace PowerUtilities
 
             if (GUILayout.Button("Select All"))
             {
-                inst.drawInfoSO.SetupRenderers(inst.gameObject, true);
+                inst.drawInfoSO.SetupRenderers(inst.gameObject);
                 var objs = inst.drawInfoSO.renders.Select(r => r.gameObject).ToArray();
                 Selection.objects = objs;
             }
