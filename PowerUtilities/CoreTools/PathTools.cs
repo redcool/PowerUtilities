f82c51865b2c8d8cf1f95bed405b5c551bc88d5f
ken <redcool007@qq.com>
2023-11-16 22:37:17 +0800

    v(2.0.16.3) update PathTools

--- PowerUtilities/CoreTools/PathTools.cs
+++ PowerUtilities/CoreTools/PathTools.cs
@@ -3,11 +3,30 @@
     using System.Collections;
     using System.Collections.Generic;
     using System.IO;
+    using System.Runtime.CompilerServices;
     using System.Text;
     using UnityEngine;
 
     public static class PathTools
     {
+        /// <summary>
+        /// dont set value
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <returns></returns>
+        public static string GetCallerFilePath([CallerFilePath]string filepath="") {
+            return filepath;
+        }
+
+        /// <summary>
+        /// dont set value
+        /// </summary>
+        /// <param name="callerFilePath"></param>
+        /// <returns></returns>
+        public static string GetCallerFolderPath([CallerFilePath] string callerFilePath="")
+        {
+            return Path.GetDirectoryName(callerFilePath);
+        }
 
         public static string GetAssetDir(string assetPath,params string[] combineStrings)
         {
