16add3e430595245b62b4752b6f9918de196ce83
gamebox1 <redcool007@qq.com>
2023-12-21 19:38:03 +0800

    v(2.0.16.47) add auto sort

--- PowerUtilities/Layers/ChildrenSortingLayerControl.cs
+++ PowerUtilities/Layers/ChildrenSortingLayerControl.cs
@@ -3,20 +3,52 @@ namespace PowerUtilities
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using System.Text;
 
 #if UNITY_EDITOR
     using UnityEditor;
+
     [CustomEditor(typeof(ChildrenSortingLayerControl))]
     public class ChildrenSortingLayerControlEditor : PowerEditor<ChildrenSortingLayerControl>
     {
-        public override bool NeedDrawDefaultUI() => true;
+        //public override bool NeedDrawDefaultUI() => true;
 
         public override void DrawInspectorUI(ChildrenSortingLayerControl inst)
         {
+            EditorGUILayout.BeginVertical("Box");
+            
+            DrawDefaultInspector();
+
             if (GUILayout.Button("Sort Children"))
             {
-                inst.OnEnable();
+                inst.StartSortChildren();
             }
+            EditorGUILayout.EndVertical();
+
+            if (inst.children == null)
+                return;
+
+            DrawStatisticsInfo(inst);
+        }
+
+        private void DrawStatisticsInfo(ChildrenSortingLayerControl inst)
+        {
+            inst.isFoldStatistics = EditorGUILayout.BeginFoldoutHeaderGroup(inst.isFoldStatistics, "StatisticsInfo");
+            if (!inst.isFoldStatistics)
+                return;
+
+            for (int i = 0; i < inst.children.Length; i++)
+            {
+                var child = inst.children[i];
+                var childInfo = inst.childrenSortingLayerNames[i];
+
+                EditorGUILayout.BeginHorizontal("Box");
+                EditorGUILayout.ObjectField(child, child.GetType(), true);
+                EditorGUILayout.LabelField(childInfo);
+                EditorGUILayout.EndHorizontal();
+            }
+
+            EditorGUILayout.EndFoldoutHeaderGroup();
         }
     }
 #endif
@@ -28,17 +60,32 @@ namespace PowerUtilities
     /// SpriteRenderer
     /// 
     /// </summary>
+    [ExecuteAlways]
     public class ChildrenSortingLayerControl : MonoBehaviour
     {
         [HelpBox()] public string HELP_STR = "Setup children's sortingLayer by children's order";
         [SortingLayerIndex] public int sortingLayerId = 0;
 
-        public Transform[] children;
+        [HideInInspector] public Transform[] children;
+        [HideInInspector] public string[] childrenSortingLayerNames;
+        [HideInInspector] public bool isFoldStatistics;
         /// <summary>
         /// sorting index in this group
         /// </summary>
         int startSortingOrder = 0;
-        public void OnEnable()
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            if (transform.hasChanged)
+            {
+                StartSortChildren();
+            }
+        }
+
+#endif
+
+        public void StartSortChildren()
         {
             startSortingOrder = 0;
             //children = GetComponentsInChildren<Transform>();
@@ -48,7 +95,9 @@ namespace PowerUtilities
                 children[i] = transform.GetChild(i);
             }
 
-            SetupChildrenSortingOrder();
+            childrenSortingLayerNames = new string[children.Length];
+
+            SetupChildrenSortingOrder(children, childrenSortingLayerNames);
         }
 
         public static SortingLayer GetSortingLayer(int id)
@@ -58,11 +107,11 @@ namespace PowerUtilities
             return SortingLayer.layers[id];
         }
 
-        public void SetupChildrenSortingOrder()
+        public void SetupChildrenSortingOrder(Transform[] children,string[] childrenSortingLayerNames)
         {
-
             var sortingLayer = GetSortingLayer(sortingLayerId);
-            sortingLayerId = sortingLayer.id;
+
+            var sb = new StringBuilder();
 
             for (int i = 0; i < children.Length; i++)
             {
@@ -82,6 +131,8 @@ namespace PowerUtilities
                     canvas.sortingOrder = startSortingOrder;
 
                     startSortingOrder++;
+
+                    sb.Append($"{canvas.sortingOrder}");
                 }
 
                 if (ps)
@@ -90,6 +141,8 @@ namespace PowerUtilities
                     psr.sortingLayerID = sortingLayer.id;
                     psr.sortingOrder = startSortingOrder;
                     startSortingOrder++;
+
+                    sb.Append($"{psr.sortingOrder}");
                 }
 
                 if (sr)
@@ -97,7 +150,14 @@ namespace PowerUtilities
                     sr.sortingLayerID = sortingLayer.id;
                     sr.sortingOrder = startSortingOrder;
                     startSortingOrder++;
+
+                    sb.Append($"{sr.sortingOrder}");
                 }
+
+                // statistics
+                sb.Insert(0,$"{sortingLayer.name} , ");
+                childrenSortingLayerNames[i] = sb.ToString();
+                sb.Clear();
             }
         }
     }
