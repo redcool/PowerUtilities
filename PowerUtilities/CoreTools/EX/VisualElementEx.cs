d612790e588c8e895323f3835ef6bbac3809092b
ken <redcool007@qq.com>
2024-01-08 22:48:45 +0800

    v(2.0.16.82) remove editor macro

--- PowerUtilities/CoreTools/EX/VisualElementEx.cs
+++ PowerUtilities/CoreTools/EX/VisualElementEx.cs
@@ -1,10 +1,15 @@
-﻿#if UNITY_EDITOR
-using UnityEngine.UIElements;
+﻿using UnityEngine.UIElements;
 
 namespace PowerUtilities.UIElements
 {
     public static class VisualElementEx
     {
+        /// <summary>
+        /// Add children foreach
+        /// </summary>
+        /// <param name="ve"></param>
+        /// <param name="isClearContainer"></param>
+        /// <param name="children"></param>
         public static void Add(this VisualElement ve, bool isClearContainer, params VisualElement[] children)
         {
             if (isClearContainer)
@@ -12,6 +17,11 @@ namespace PowerUtilities.UIElements
             children.ForEach(child => { if (child != null) ve.Add(child); });
         }
 
+        /// <summary>
+        /// Get parent element upforwards when its parent is null
+        /// </summary>
+        /// <param name="ve"></param>
+        /// <returns></returns>
         public static VisualElement GetRootElement(this VisualElement ve)
         {
             var p = ve.parent;
@@ -24,4 +34,3 @@ namespace PowerUtilities.UIElements
         }
     }
 }
-#endif
