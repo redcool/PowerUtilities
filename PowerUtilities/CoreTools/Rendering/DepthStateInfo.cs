e258f3dbe26ad008b23c3a0c94d3ff29f25e8c83
gamebox1 <redcool007@qq.com>
2024-01-29 16:42:17 +0800

    v(2.0.17.44) add DepthStateInfo.cs

---
+++ PowerUtilities/CoreTools/Rendering/DepthStateInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Rendering;
+
+namespace PowerUtilities
+{
+    /// <summary>
+    /// override depth state 
+    /// </summary>
+    [Serializable]
+    public class DepthStateInfo
+    {
+        public bool isOverrideDepthState;
+        public bool isWriteDepth = true;
+        public CompareFunction compareFunc = CompareFunction.LessEqual;
+    }
+}
