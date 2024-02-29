1699c964495a93c13e427ec9701a5a8ea8f78f62
gamebox1 <redcool007@qq.com>
2024-02-29 12:15:34 +0800

    v(2.0.17.68) update DrawShadow GetTemporaryShadowTexture

---
+++ PowerUtilities/CoreTools/EX/RenderTextureTools.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace PowerUtilities
+{
+    public static class RenderTextureTools
+    {
+
+        public static readonly bool m_ForceShadowPointSampling;
+
+        static RenderTextureTools()
+        {
+            m_ForceShadowPointSampling = SystemInfo.graphicsDeviceType == GraphicsDeviceType.Metal &&
+                GraphicsSettings.HasShaderDefine(Graphics.activeTier, BuiltinShaderDefine.UNITY_METAL_SHADOWS_USE_POINT_FILTERING);
+        }
+
+        public static RenderTexture GetTemporaryShadowTexture(int width, int height, int bits)
+        {
+            var format = GraphicsFormatUtility.GetDepthStencilFormat(bits, 0);
+            RenderTextureDescriptor rtd = new RenderTextureDescriptor(width, height, GraphicsFormat.None, format);
+            rtd.shadowSamplingMode = (RenderingUtils.SupportsRenderTextureFormat(RenderTextureFormat.Shadowmap)
+                && (SystemInfo.graphicsDeviceType != GraphicsDeviceType.OpenGLES2)) ?
+                ShadowSamplingMode.CompareDepths : ShadowSamplingMode.None;
+            var shadowTexture = RenderTexture.GetTemporary(rtd);
+            shadowTexture.filterMode = m_ForceShadowPointSampling ? FilterMode.Point : FilterMode.Bilinear;
+            shadowTexture.wrapMode = TextureWrapMode.Clamp;
+            return shadowTexture;
+        }
+    }
+}
