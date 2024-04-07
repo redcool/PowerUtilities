e0aa6f54b7636b47a0d00ff27dd00e945bbe394d
gamebox1 <redcool007@qq.com>
2024-04-07 18:37:04 +0800

    v(2.0.18.23) add InstanceManager.cs

---
+++ PowerUtilities/CoreTools/Managers/InstanceManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerUtilities
+{
+    /// <summary>
+    /// for Manager objects(like monoBehaviour instance)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class InstanceManager<T>
+    {
+        // trace instances
+        List<T> instanceList = new List<T>();
+        T topInstance;
+
+        public void Add(T instance)
+        {
+            instanceList.Add(instance);
+            topInstance = instanceList[instanceList.Count - 1];
+        }
+
+        public void Remove(T instance)
+        {
+            topInstance = default;
+            instanceList.Remove(instance);
+
+            if (instanceList.Count > 0)
+                topInstance = instanceList[instanceList.Count - 1];
+        }
+
+        /// <summary>
+        /// Get topInstance or defaultInst 
+        /// </summary>
+        /// <param name="defaultInst"> topInstance not exist use defaultInst </param>
+        /// <returns></returns>
+        public T GetTopInstance(T defaultInst)
+            => topInstance ?? defaultInst;
+
+        public bool IsUsed(T inst)
+            => topInstance.Equals(inst);
+
+        public List<T> InstanceList
+            => instanceList;
+    }
+}
