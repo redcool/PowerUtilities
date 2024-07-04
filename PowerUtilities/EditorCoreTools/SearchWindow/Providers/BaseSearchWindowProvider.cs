7c91def1cf2a27afde2df2b6f66627550dfb18d7
gamebox1 <redcool007@qq.com>
2024-07-04 10:55:11 +0800

    v(2.0.19.42) update ,editor condition

--- PowerUtilities/EditorCoreTools/SearchWindow/Providers/BaseSearchWindowProvider.cs
+++ PowerUtilities/EditorCoreTools/SearchWindow/Providers/BaseSearchWindowProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -35,3 +36,4 @@ namespace PowerUtilities
         }
     }
 }
+#endif
