50cc36a82134b36ebf01403828961cef24049d2b
gamebox1 <redcool007@qq.com>
2024-07-15 18:57:34 +0800

    v(2.0.19.59) add MainLightShadowCaster.cs

---
+++ PowerUtilities/EditorCoreTools/SerializedObjectTools.cs
@@ -0,0 +1,46 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace PowerUtilities
+{
+    public static class SerializedObjectTools
+    {
+        public static Dictionary<Object, SerializedObject> soDict = new Dictionary<Object, SerializedObject>();
+
+        /// <summary>
+        /// Get SerializedObject with cached
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static SerializedObject GetObject(Object obj)
+        {
+            var so = DictionaryTools.Get(soDict,obj,obj=> new SerializedObject(obj));
+            if (so.targetObject != obj)
+                so = soDict[obj] = new SerializedObject(obj);
+            return so;
+        }
+
+        /// <summary>
+        /// Get a property from keyObj's SerializedObject,
+        /// </summary>
+        /// <param name="keyObj"></param>
+        /// <param name="propName"></param>
+        /// <returns></returns>
+        public static SerializedProperty GetProperty(Object keyObj,string propName)
+        {
+            if (!keyObj || string.IsNullOrEmpty(propName))
+                return default;
+
+            var prop = GetObject(keyObj).FindProperty(propName);
+            return prop;
+        }
+    }
+}
+#endif
---
+++ PowerUtilities/SFCFramework/Framework/Utils/ShadowUtilsEx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace PowerUtilities
+{
+    public static class ShadowUtilsEx
+    {
+
+        public static float SoftShadowQualityToShaderProperty(Light light, bool softShadowsEnabled)
+        {
+            float softShadows = softShadowsEnabled ? 1.0f : 0.0f;
+            if (light.TryGetComponent(out UniversalAdditionalLightData additionalLightData))
+            {
+                var softShadowQuality = (additionalLightData.softShadowQuality == SoftShadowQuality.UsePipelineSettings)
+                    //? UniversalRenderPipeline.asset?.softShadowQuality
+                    ? (SoftShadowQuality)SerializedObjectTools.GetProperty(UniversalRenderPipeline.asset, "m_SoftShadowQuality").enumValueIndex
+                    : additionalLightData.softShadowQuality;
+                softShadows *= Math.Max((int)softShadowQuality, (int)SoftShadowQuality.Low);
+            }
+
+            return softShadows;
+        }
+
+        /// copy from ShadowUtils
+        public static void GetScaleAndBiasForLinearDistanceFade(float fadeDistance, float border, out float scale, out float bias)
+        {
+            // To avoid division from zero
+            // This values ensure that fade within cascade will be 0 and outside 1
+            if (border < 0.0001f)
+            {
+                float multiplier = 1000f; // To avoid blending if difference is in fractions
+                scale = multiplier;
+                bias = -fadeDistance * multiplier;
+                return;
+            }
+
+            border = 1 - border;
+            border *= border;
+
+            // Fade with distance calculation is just a linear fade from 90% of fade distance to fade distance. 90% arbitrarily chosen but should work well enough.
+            float distanceFadeNear = border * fadeDistance;
+            scale = 1.0f / (fadeDistance - distanceFadeNear);
+            bias = -distanceFadeNear / (fadeDistance - distanceFadeNear);
+        }
+    }
+}
