7dcc6343288491754caf70bef8edca05a604c673
gamebox1 <redcool007@qq.com>
2024-07-15 15:09:03 +0800

    v(2.0.19.58) update RenderPipelineTools

--- PowerUtilities/CoreTools/Rendering/RenderPipelineTools.cs
+++ PowerUtilities/CoreTools/Rendering/RenderPipelineTools.cs
@@ -2,13 +2,16 @@ namespace PowerUtilities
 {
     using System.Collections;
     using System.Collections.Generic;
+    using UnityEditor;
     using UnityEngine;
     using UnityEngine.Rendering;
     using UnityEngine.Rendering.Universal;
 
     public static class RenderPipelineTools
     {
-        public static bool IsUniversalPipeline() =>  GraphicsSettings.currentRenderPipeline.GetType().Name.IsMatch("UniversalRenderPipelineAsset", StringEx.NameMatchMode.Contains);
+
+
+        public static bool IsUniversalPipeline() => GraphicsSettings.currentRenderPipeline.GetType().Name.IsMatch("UniversalRenderPipelineAsset", StringEx.NameMatchMode.Contains);
         public static bool IsHDRenderPipeline() => GraphicsSettings.currentRenderPipeline.GetType().Name.IsMatch("HDRenderPipelineAsset", StringEx.NameMatchMode.Contains);
 
         static UniversalRenderPipelineAsset urpAsset;
@@ -20,6 +23,41 @@ namespace PowerUtilities
                     urpAsset = UniversalRenderPipeline.asset;
                 return urpAsset;
             }
+        }
+
+
+        [RuntimeInitializeOnLoadMethod]
+#if UNITY_EDITOR
+        [InitializeOnLoadMethod]
+#endif
+        static void Init()
+        {
+            RenderPipelineManager.beginContextRendering -= RenderPipelineManager_beginContextRendering;
+            RenderPipelineManager.beginContextRendering += RenderPipelineManager_beginContextRendering;
+        }
+        private static void RenderPipelineManager_beginContextRendering(ScriptableRenderContext context, List<Camera> list)
+        {
+            CameraList = list;
+        }
+
+        /// <summary>
+        /// Camera list for urp rendering
+        /// </summary>
+        public static List<Camera> CameraList { get; private set; }
+
+        /// <summary>
+        /// Get next in camera list, when nextId>=cameraList count return 0
+        /// </summary>
+        /// <param name="curCamera"></param>
+        /// <param name="nextCamId"></param>
+        /// <returns></returns>
+        public static Camera GetNextCamera(Camera curCamera, out int nextCamId)
+        {
+            nextCamId = CameraList.IndexOf(curCamera) + 1;
+            if (nextCamId >= CameraList.Count)
+                nextCamId = 0;
+            var nextCam = CameraList[nextCamId];
+            return nextCam;
         }
     }
 }
