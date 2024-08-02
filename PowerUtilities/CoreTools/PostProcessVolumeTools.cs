5d13b875d2d02648e9e56d9eab1122dd1a2f680e
gamebox1 <redcool007@qq.com>
2024-08-02 17:10:26 +0800

    v(2.0.19.74) add PostProcessVolumeTools.cs

---
+++ PowerUtilities/CoreTools/PostProcessVolumeTools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace PowerUtilities
+{
+    /// <summary>
+    /// PostProcessing Volume Tools
+    /// </summary>
+    public static class PostProcessVolumeTools
+    {
+        public static Volume[] GetGlobalVolumes(LayerMask layerMask)
+        => VolumeManager.instance.GetVolumes(layerMask).Where(v => v.isGlobal).ToArray();
+        
+        public static Volume GetFirstGlobalVolume(LayerMask layerMask)
+            => VolumeManager.instance.GetVolumes(layerMask).Where(v => v.isGlobal).FirstOrDefault();
+
+    }
+}
