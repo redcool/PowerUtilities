3cfc1655d0493d4f2ae3c797442f8f6fb85a35b5
gamebox1 <redcool007@qq.com>
2024-08-09 11:57:55 +0800

    v(2.0.19.78) add VolumeEx.cs

---
+++ PowerUtilities/CoreTools/Rendering/URP/VolumeEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Rendering;
+
+namespace PowerUtilities
+{
+    public static class VolumeEx
+    {
+        /// <summary>
+        /// Get v.profile or v.sharedProfile
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static VolumeProfile GetProfile(this Volume v)
+        => v.HasInstantiatedProfile() ? v.profile : v.sharedProfile;
+    }
+}
