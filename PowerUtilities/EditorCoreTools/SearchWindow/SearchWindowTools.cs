0326390e5ec5a7a45fa503f351fce8ababe84425
gamebox1 <redcool007@qq.com>
2024-12-21 20:53:35 +0800

    v(2.0.21.41) remove unused ns

--- PowerUtilities/EditorCoreTools/SearchWindow/SearchWindowTools.cs
+++ PowerUtilities/EditorCoreTools/SearchWindow/SearchWindowTools.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Unity.VisualScripting.YamlDotNet.Serialization.TypeResolvers;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
