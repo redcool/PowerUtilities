0bfd17aaa05f740bb71b26204da037075099cb6f
gamebox1 <redcool007@qq.com>
2025-01-17 15:52:51 +0800

    v(2.0.21.77) add UNITY_EDITOR

--- PowerUtilities/EditorCoreTools/ProjectSettingsAPI/Managers/UnityTagManager.cs
+++ PowerUtilities/EditorCoreTools/ProjectSettingsAPI/Managers/UnityTagManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
@@ -123,3 +124,4 @@ namespace PowerUtilities
         }
     }
 }
+#endif
