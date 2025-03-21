d1471e49aeca013e47d4f614304bbd0c8a525eba
gamebox1 <redcool007@qq.com>
2025-03-21 16:15:36 +0800

    v(2.0.21.124) add PresetTools.cs ApplyPresetsToSelected.cs

---
+++ PowerUtilities/EditorCoreTools/PresetTools.cs
@@ -0,0 +1,50 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.Presets;
+using Object = UnityEngine.Object;
+
+namespace PowerUtilities
+{
+    public static class PresetTools
+    {
+        /// <summary>
+        /// Get all defaultPresets by defaultTypes
+        /// </summary>
+        /// <returns></returns>
+        public static DefaultPreset[] GetAllDefaultPresets()
+        {
+            var types = Preset.GetAllDefaultTypes();
+            return types.SelectMany(t => Preset.GetDefaultPresetsForType(t))
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Try apply first default preset to object
+        /// </summary>
+        /// <param name="presets"></param>
+        /// <param name="obj"></param>
+        public static bool TryApplyDefaultPreset(Object obj, params DefaultPreset[] presets)
+        {
+            if (obj == null)
+                return false;
+
+            if(presets == null || presets.Length == 0)
+                presets = GetAllDefaultPresets();
+
+            foreach (var defaultPreset in presets)
+            {
+                if (defaultPreset.enabled )
+                {
+                    if (defaultPreset.preset.ApplyTo(obj))
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+#endif
---
+++ PowerUtilities/Presets/ApplyPresetsToSelected.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.Presets;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace PowerUtilities
+{
+    /// <summary>
+    /// Apply Preset for selected items
+    /// </summary>
+    public class ApplyPresetsToSelected
+    {
+        const string ROOT_MENU = "PowerUtilities/Presets/";
+        const string APPLY_PRESET_TO_SELECTED = ROOT_MENU + "Apply Preset To Selected Folder";
+
+        [MenuItem(APPLY_PRESET_TO_SELECTED)]
+        static void ApplyPresetsToFolderItems()
+        {
+            var defaultPresets= PresetTools.GetAllDefaultPresets();
+
+            Selection.GetFiltered<Object>(SelectionMode.DeepAssets).ForEach(obj =>
+            {
+                var imp = TextureImporter.GetAtPath(AssetDatabase.GetAssetPath(obj));
+                var isDone = PresetTools.TryApplyDefaultPreset(imp, defaultPresets);
+            });
+        }
+    }
+}
+#endif
