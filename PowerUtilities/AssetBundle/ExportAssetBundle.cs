0dac6b55a2a491e1eee3ee1e691564ac4319b96d
gamebox1 <redcool007@qq.com>
2025-08-21 12:08:06 +0800

    v(2.0.23.44) update : build active target

--- PowerUtilities/AssetBundle/ExportAssetBundle.cs
+++ PowerUtilities/AssetBundle/ExportAssetBundle.cs
@@ -11,12 +11,12 @@ namespace PowerUtilities
         public const string ASSET_BUNDLE_UTILS = "PowerUtilities/AssetBundleUtils";
         const string BUNDLE_PATH = "Assets/../Bundles";
 
-        [MenuItem(ASSET_BUNDLE_UTILS + "/BuildBundles (android)")]
+        [MenuItem(ASSET_BUNDLE_UTILS + "/BuildBundles")]
         static void BuildBundles()
         {
             CreateDir(BUNDLE_PATH);
-
-            BuildPipeline.BuildAssetBundles(BUNDLE_PATH, BuildAssetBundleOptions.None, BuildTarget.Android);
+            var target = EditorUserBuildSettings.activeBuildTarget;
+            BuildPipeline.BuildAssetBundles(BUNDLE_PATH, BuildAssetBundleOptions.None, target);
         }
 
         private static void CreateDir(string path)
@@ -27,14 +27,14 @@ namespace PowerUtilities
             }
         }
 
-        [MenuItem(ASSET_BUNDLE_UTILS + "/BuildSelected (android)")]
+        [MenuItem(ASSET_BUNDLE_UTILS + "/BuildSelected")]
         static void BuildSelected()
         {
             var obj = Selection.activeObject;
             if (!obj)
                 return;
-
-            AssetDatabaseTools.BuildObjectsAssetBundle(obj.name, buildTarget: BuildTarget.Android, bundlePath: BUNDLE_PATH, objects: Selection.objects);
+            var target = EditorUserBuildSettings.activeBuildTarget;
+            AssetDatabaseTools.BuildObjectsAssetBundle(obj.name, buildTarget: target, bundlePath: BUNDLE_PATH, objects: Selection.objects);
         }
     }
 }
