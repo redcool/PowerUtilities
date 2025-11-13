44da7a20b447fe218d783e7caee3ef6cf88ff7c1
gamebox1 <redcool007@qq.com>
2025-11-13 13:16:22 +0800

    v(2.0.23.101) add : BaseEditorWindow.cs

---
+++ PowerUtilities/EditorTools/EditorWindows/BaseEditorWindow.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+namespace PowerUtilities
+{
+    /// <summary>
+    /// Base EditorWindow
+    /// Show scriptObj (ObjectField)
+    /// </summary>
+    public class BaseEditorWindow : EditorWindow
+    {
+        public Object scriptObj;
+
+        public virtual void OnEnable()
+        {
+            scriptObj = AssetDatabaseTools.FindAssetPathAndLoad<Object>(out _, GetType().Name);    
+        }
+
+        public virtual void OnGUI()
+        {
+            EditorGUITools.DrawDisableScope(true, () =>
+            {
+                EditorGUILayout.ObjectField(GUIContentEx.TempContent("Script : ", "Script draw gui "), scriptObj, typeof(Object), false);
+            });
+
+        }
+    }
+
+}
+#endif
