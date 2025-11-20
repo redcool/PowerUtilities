312ad02d3e3a0d8d0cbbbe4011aa7e99306e468a
gamebox1 <redcool007@qq.com>
2025-11-20 13:25:47 +0800

    v(2.0.23.111) update : AnimationTools

--- PowerUtilities/CoreTools/EX/AnimationTools.cs
+++ PowerUtilities/CoreTools/EX/AnimationTools.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 
 namespace PowerUtilities
@@ -17,15 +20,34 @@ namespace PowerUtilities
             }
             return null;
         }
-
+        /// <summary>
+        /// Get AnimationClips from Animation
+        /// in EditorMode , iterator m_Animations
+        /// in Player solo, iterator AnimationState
+        /// </summary>
+        /// <param name="anim"></param>
+        /// <returns></returns>
         public static AnimationClip[] GetAnimationClips(this Animation anim)
         {
             var list = new List<AnimationClip>();
+#if UNITY_EDITOR
+            var animSO = new SerializedObject(anim);
+            var arrayProp = animSO.FindProperty("m_Animations");
+            for (int i = 0; i < arrayProp.arraySize; i++)
+            {
+                var clipProp = arrayProp.GetArrayElementAtIndex(i);
+                if (clipProp.objectReferenceValue is AnimationClip clip)
+                    list.Add(clip);
+            }
+#else
             foreach (AnimationState state in anim)
             {
                 list.Add(state.clip);
             }
+#endif
+
             return list.ToArray();
+
         }
     }
 }
