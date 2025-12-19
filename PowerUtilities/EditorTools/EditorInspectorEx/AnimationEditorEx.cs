af7d9fdd71c46688bbe8881bea925b1369381347
gamebox1 <redcool007@qq.com>
2025-12-19 12:35:37 +0800

    v(2.0.23.124) add: AnimationEditorEx

---
+++ PowerUtilities/EditorTools/EditorInspectorEx/AnimationEditorEx.cs
@@ -0,0 +1,44 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+
+namespace PowerUtilities
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(Animation))]
+    public class AnimationEditorEx : BaseEditorEx
+    {
+        public override string GetDefaultInspectorTypeName() => "UnityEditor.AnimationEditor";
+
+        public bool isBakeAnimTextureFolded;
+
+        public bool isAnimTextureSupported;
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            isAnimTextureSupported = AssetDatabaseTools.FindAssetPathAndLoad<TextAsset>(out var _, "AnimTextureBakeSettings", ".cs");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var inst= (target as Animation);
+
+            if (isAnimTextureSupported && GUILayout.Button("Add BakeAnimTexture"))
+            {
+                var type = ReflectionTools.GetAppDomainTypes<MonoBehaviour>(type => type.FullName == "AnimTexture.BakeAnimTexture")
+                    .FirstOrDefault();
+                if (type != null)
+                    inst.gameObject.GetOrAddComponent(type);
+            }
+        }
+
+    }
+}
+#endif
