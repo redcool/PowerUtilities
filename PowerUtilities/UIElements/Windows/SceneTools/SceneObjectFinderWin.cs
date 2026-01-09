5008599dd8547d2618af52a5841dc073ae873e6b
gamebox1 <redcool007@qq.com>
2026-01-09 18:14:04 +0800

    v(2.0.23.148) update : SceneObjectFinderWin

--- PowerUtilities/UIElements/Windows/SceneTools/SceneObjectFinderWin.cs
+++ PowerUtilities/UIElements/Windows/SceneTools/SceneObjectFinderWin.cs
@@ -1,8 +1,9 @@
-#if UNITY_EDITOR
+﻿#if UNITY_EDITOR
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
@@ -28,6 +29,14 @@ namespace PowerUtilities
         {
             root.Q<Button>("bt_find").clicked -= FindObjects;
             root.Q<Button>("bt_find").clicked += FindObjects;
+
+            root.Q<Button>("bt_clear").clicked -= Clear ;
+            root.Q<Button>("bt_clear").clicked += Clear;
+        }
+
+        void Clear()
+        {
+            Selection.activeGameObject = null;
         }
 
         private void FindObjects()
@@ -38,6 +47,11 @@ namespace PowerUtilities
             var componentTypeNames = rootVisualElement.Q<TextField>("ComponentTypeNames").value.SplitBy();
             var objNames = rootVisualElement.Q<TextField>("ObjectNames").value.SplitBy();
 
+            var isLayerFind = rootVisualElement.Q<Toggle>("IsLayerFind").value;
+            var isTagFind = rootVisualElement.Q<Toggle>("IsTagFind").value;
+            var isComponentTypeNames = rootVisualElement.Q<Toggle>("IsComponentTypeNames").value;
+            var isObjectNames = rootVisualElement.Q<Toggle>("IsObjectNames").value;
+
             var parentGo = Selection.activeGameObject;
 
             var trs =
@@ -45,10 +59,14 @@ namespace PowerUtilities
                 //FindObjectsByType<Transform>(includeInactive ? FindObjectsInactive.Include : FindObjectsInactive.Exclude, FindObjectsSortMode.None);
                 FindObjectsOfType<Transform>(includeInactive);
 
-            var gos = trs.Where(tr => tr.gameObject.layer == layer)
-                .Where(tr => tr.CompareTag(tag))
-                .Where(tr => (componentTypeNames.Length>0 ? componentTypeNames.Where(comName => tr.GetComponent(comName)).Count() > 0 : true))
-                .Where(tr => (objNames.Length > 0 ? objNames.Where(objName => tr.gameObject.name.Contains(objName)).Count() > 0 : true))
+            var gos = trs.Where(tr => isLayerFind ? tr.gameObject.layer == layer : true)
+                .Where(tr => isTagFind ? tr.CompareTag(tag) : true)
+                .Where(tr => isComponentTypeNames ?
+                    (componentTypeNames.Length > 0 ? componentTypeNames.Where(comName => tr.GetComponent(comName)).Count() > 0 : true) : true
+                    )
+                .Where(tr => isObjectNames ?
+                (objNames.Length > 0 ? objNames.Where(objName => tr.gameObject.name.Contains(objName)).Count() > 0 : true) : true
+                )
                 .Select(tr => tr.gameObject)
                 .ToArray();
 
